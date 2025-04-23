3158e803620a86b3b278bec150bf78378b5efaab
jamesnizama <jamesnizama20@gmail.com>
2025-04-23 11:56:04 -0500

    Inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PanelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
